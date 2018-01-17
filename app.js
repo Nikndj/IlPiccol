6f4d764dc69a953df351cc9e10617a1cbc628ae6
Nikndj <nicola.delgiudice@studenti.unicam.it>
2018-01-17 12:10:08 +0100

    Aggiunta pacchetti passport e body-parser. Aggiunta di auth

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var express = require('express'),
  Utente = require('./models/utenti'),
  Fornitore = require('./models/fornitori'),
  Prodotto = require('./models/prodotti'),
+ bodyParser= require("body-parser"),
+ passport = require('passport'),
+ localStrategy= require('passport-local');
+ passportLocalMongoose= require('passport-local-mongoose');
  app = express();
 
 
@@ -17,12 +21,21 @@ mongoose.connect('mongodb://localhost/ilpiccoldb');
 //app.engine('html', ejs.renderFile);
 //app.set('view engine', 'html');
 
-
+//APP SETTINGS
 app.set('view engine', 'ejs');
 //use per specificare la cartella in cui si trovano i file statici (css, immagini...)
 app.use(express.static('../public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-//routes
+passport.use(new localStrategy(Utente.authenticate()));
+passport.serializeUser(Utente.serializeUser());
+passport.deserializeUser(Utente.deserializeUser());
+
+
+
+//ROUTES
 app.get('/', function(req, res) {
 	res.render('Homepage.ejs');
 });
@@ -33,10 +46,6 @@ app.get('/contacts', function(req, res) {
 
 app.get('/login', function(req, res) {
 	res.render('Accesso.ejs');
-});
-
-app.get('/register', function(req, res) {
-	res.render('Registrazione.ejs');
 });
 
 app.get('/support', function(req, res) {
@@ -54,6 +63,28 @@ app.get('/catalog', function(req, res) {
 app.get("*", function(req, res) {
 	res.send("Che cazzo ce stai a fa qua ao");
 });
+
+//REGISTRATION ROUTES
+app.get('/register', function(req, res) {
+	res.render('Registrazione.ejs');
+});
+
+app.post("/register", function(req, res){
+	req.body.nomeUtente
+	req.body.password
+	req.body.email
+	Utente.register(new Utente({ nomeUtente: req.body.nomeUtente}), req.body.password, req.body.email, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("/register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("*");
+		});
+	});
+});
+
+//LOGIN ROUTES
 
 //per indicare su che porta deve ascoltare il server
 app.listen(3000, function() {
--- models/utenti.js
+++ models/utenti.js
@@ -5,12 +5,14 @@ var schemaUtente = new Schema({
 	nomeUtente: String,
 	password: String,
 	email: String, 
+	/*
 	nome: String,
 	cognome: String,
 	dataNascita: Date,
 	indirizzo: String,
 	cap: Number,
 	citta: String
+	*/
 });
 
 module.exports = mongoose.model('Utente', schemaUtente);
