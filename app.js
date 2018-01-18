49c9f52da0f1f7a36c1a1558b3ac85e3b09142ac
Leo <linkinday@mail.com>
2018-01-18 17:52:59 +0100

    Risoluzioni problemi app.js

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ var express = require('express'),
  se si è lasciato il path di default nell'installazione)*/
 mongoose.connect('mongodb://localhost/ilpiccoldb');
 
-////set per cambiare la cartella standard delle view
-//app.set('views', '../Html');
-////engine e set per cambiare l'engine di base da .ejs a .html
-//app.engine('html', ejs.renderFile);
-//app.set('view engine', 'html');
-
 //APP SETTINGS
 app.set('view engine', 'ejs');
 //use per specificare la cartella in cui si trovano i file statici (css, immagini...)
@@ -32,7 +26,6 @@ app.use(passport.session());
 passport.use(new localStrategy(Utente.authenticate()));
 passport.serializeUser(Utente.serializeUser());
 passport.deserializeUser(Utente.deserializeUser());
-
 
 
 //ROUTES
@@ -56,39 +49,40 @@ app.get('/catalog', function(req, res) {
 	res.render('Catalogo.ejs');
 });
 
-app.get("*", function(req, res) {
-	res.send("Che cazzo ce stai a fa qua ao");
-});
-
 //REGISTRATION ROUTES
 app.get('/register', function(req, res) {
 	res.render('Registrazione.ejs');
 });
 
-app.post("/register", function(req, res){
-	req.body.nomeUtente
-	req.body.password
-	req.body.email
-	Utente.register(new Utente({ nomeUtente: req.body.nomeUtente}), req.body.password, req.body.email, function(err, user){
-		if(err){
-			console.log(err);
-			return res.render("/register");
-		}
-		passport.authenticate("local")(req, res, function(){
-			res.redirect("*");
+app.post("/register", function(req, res) {
+	//req.body.nomeUtente
+	var pass = req.body.password
+	var passRepeat = req.body.passwordRepeat
+	//req.body.email
+	if (pass === passRepeat) {
+		Utente.register(new Utente({nomeUtente: req.body.nomeUtente, email: req.body.email}), req.body.password, function(err, user){
+			if(err){
+				console.log(err);
+				return res.render("Registrazione.ejs");
+			}
+			passport.authenticate("local")(req, res, function(){
+				res.redirect("*");
+			});
 		});
-	});
+	} else {
+		res.redirect("Registrazione.ejs");
+		console.log("Le password non coincidono");
+	}
 });
 
 //LOGIN ROUTES
-
 app.get('/login', function(req, res) {
 	res.render('Accesso.ejs');
 });
 
 app.post("/login", passport.authenticate("local",{
-    successRedirect: "/secret",
-    failureRedirect: "/login"
+    successRedirect: "secret.ejs",
+    failureRedirect: "Accesso.ejs"
 }), function (req, res) {
 });
 
@@ -103,6 +97,12 @@ function isLoggedIn(req, res, next) {
 	}
 	res.redirect("/login");
 }
+
+//deve rimanere in fondo altrimenti le altre routes non funzionano
+app.get("*", function(req, res) {
+	res.send("Che cazzo ce stai a fa qua ao");
+});
+
 
 //per indicare su che porta deve ascoltare il server
 app.listen(3000, function() {
